7b19ba5bf42429e8a51dbddb36118e349f45cfb4
NLight41 <oaonathan0410@gmail.com>
2025-03-10 18:35:27 +0800

    上傳 Variables.cpp 至 GitHub

---
+++ CppLearning/01-Basics/Variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+//試試分別使用 using 來宣告 std 標準函式庫中的 cin、cout 等函式
+ 
+ int main(){
+ 	
+ 	int age;
+ 	cout << "請輸入您的年齡: ";
+ 	cin >> age;
+ 	cout << "你的年齡是: " << age << " 歲" << endl;
+	return 0;
+	
+ } 
+ 
+ // 嘗試基本的輸入輸出、宣告變數等 
